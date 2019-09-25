35a4eba8a89b84b6624e46b55c0899a3fa864271
Olivier Romanetti <olivier.romanetti-extern@renault.com>
2019-09-25 15:01:12 +0200

    Pipe as provider in the component instead of being used in the template directly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSizePipe } from './filesize.pipe';
 
 interface File {
   name: string;
-  size: number;
+  size: any;
   type: string;
 }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [FileSizePipe],
 })
 export class AppComponent implements OnInit {
   files: File[];
+  mapped: File[];
   title = 'custom-pipe';
 
+  constructor(private filesize: FileSizePipe) {}
   ngOnInit() {
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' },
     ];
+
+    this.mapped = this.files.map(file => {
+      return {
+        name: file.name,
+        type: file.type,
+        size: this.filesize.transform(file.size, 'mb'),
+      };
+    });
   }
 }
